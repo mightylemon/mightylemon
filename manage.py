a058b66a18d47cd2bc3f81762bc4691380105ade
Brian Rosner <brosner@gmail.com>
2007-12-12 14:05:25 -0700

    improved error handling in manage.py

--- manage.py
+++ manage.py
@@ -2,9 +2,9 @@
 from django.core.management import execute_manager
 try:
     import settings # assumed to be in the same directory.
-except ImportError:
+except ImportError, e:
     import sys
-    sys.stderr.write("canot find settings.")
+    sys.stderr.write("canot find settings: %s\n" % e)
     sys.exit(1)
 
 if __name__ == "__main__":
