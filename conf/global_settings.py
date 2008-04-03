e13e08832941e7e30d37dd727f91259eae583af0
brosner <brosner@gmail.com>
2008-04-02 19:03:38 -0600

    Turn off the CacheMiddleware until I can break out the logger and my blog into two processes.

--- conf/global_settings.py
+++ conf/global_settings.py
@@ -58,7 +58,6 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 )
 
 MIDDLEWARE_CLASSES = (
-    "django.middleware.cache.CacheMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
