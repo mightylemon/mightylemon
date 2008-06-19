cd5a653dadfac899118d14af92eaa2be95b0b003
brosner <brosner@gmail.com>
2008-06-18 19:51:58 -0600

    Automatically determine the MEDIA_ROOT.

--- conf/global_settings.py
+++ conf/global_settings.py
@@ -31,7 +31,7 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ""
+MEDIA_ROOT = os.path.realpath(os.path.join(os.path.dirname(__file__), "../static"))
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
