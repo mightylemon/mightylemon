6c962e9da590692548c71dfa95fb34dbe96b4973
brosner <brosner@gmail.com>
2008-08-11 19:08:25 -0600

    Changed INSTALLED_APPS entry from app name to full project + app name since #5825 has been fixed in Django.

--- conf/global_settings.py
+++ conf/global_settings.py
@@ -75,9 +75,7 @@ TEMPLATE_DIRS = (
 INSTALLED_APPS = (
    "oebfare.blog",
    "oebfare.links",
-   # See #5825 and #6128 about why apps with management commands must be
-   # referenced without the project in the import path.
-   "aggregator",
+   "oebfare.aggregator",
    
    "tagging",
    "comment_utils",
