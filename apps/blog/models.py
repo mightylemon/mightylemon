1ba995da35c5facd9d3289b189774f33fe9a4437
Brian Rosner <brosner@gmail.com>
2008-12-10 01:47:05 -0700

    Added django-mailer support for comment e-mails.

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -2,10 +2,14 @@
 from datetime import datetime
 
 from django.db import models
+from django.conf import settings
+from django.template import Context, loader
+from django.contrib.sites.models import Site
 from django.utils.translation import ugettext_lazy as _
 
 from tagging.models import Tag
 from tagging.fields import TagField
+from mailer import send_mail
 from comment_utils.moderation import CommentModerator, moderator
 
 
@@ -44,5 +48,25 @@ class Post(models.Model):
 class PostModerator(CommentModerator):
     akismet = True
     email_notification = True
+    
+    def email(self, comment, content_object):
+        """
+        Use django-mailer for mail delivery.
+        """
+        if not self.email_notification:
+            return
+        recipient_list = [manager_tuple[1] for manager_tuple in settings.MANAGERS]
+        t = loader.get_template("comment_utils/comment_notification_email.txt")
+        ctx = Context({
+            "comment": comment,
+            "content_object": content_object,
+            "site": Site.objects.get_current(),
+        })
+        subject = '[%s] Comment: "%s"' % (
+            Site.objects.get_current().name,
+            content_object,
+        )
+        message = t.render(ctx)
+        send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, recipient_list)
 
 moderator.register(Post, PostModerator)
--- settings.py
+++ settings.py
@@ -78,6 +78,7 @@ INSTALLED_APPS = (
    "wsgi",
    
    "tagging",
+   "mailer",
    "comment_utils",
    
    "django.contrib.admin",
