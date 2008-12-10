961005d3569dfc65f4183694d866d5c71e1eec11
Brian Rosner <brosner@gmail.com>
2008-12-10 00:27:33 -0700

    Added active field to list_display and list_filter of PostAdmin to improve it slightly.

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -3,7 +3,8 @@ from django.contrib import admin
 from blog.models import Post
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ("id", "title", "pub_date")
+    list_display = ("id", "title", "pub_date", "active")
+    list_filter = ("active",)
     list_display_links = ("id", "title")
     search_fields = ("title", "text")
     prepopulated_fields = {"slug": ("title",)}
