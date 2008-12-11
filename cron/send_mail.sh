483ad991e670a91feb025b7c212c4875d5064b71
Brian Rosner <brosner@gmail.com>
2008-12-10 18:51:26 -0700

    Added some shell scripts to deal with django-mailer commands from the cron.

---
+++ cron/send_mail.sh
@@ -0,0 +1,9 @@
+
+# cheat and hard-code the path.
+PROJECT_ROOT=/home/brian/webapps/oebfare
+
+# activate the oebfare virtualenv
+source /home/oebfare/.virtualenv/oebfare/bin/activate
+
+cd $PROJECT_ROOT
+python manage.py send_mail >> $PROJECT_ROOT/logs/cron_mail.log 2>&1
