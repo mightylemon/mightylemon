1f463baefe7946c35c21b2e0223f94d37a8b7ad0
Justin Lilly <jlilly@grafnik.local>
2009-02-12 09:05:24 -0500

    Adding in a specialized view for comments in the admin

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,6 +1,7 @@
-
 from django.contrib import admin
 from blog.models import Post
+from django.contrib.comments.admin import CommentsAdmin
+from django.contrib.comments.models import Comment
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ("id", "title", "pub_date", "active")
@@ -10,3 +11,21 @@ class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
 
 admin.site.register(Post, PostAdmin)
+
+admin.site.unregister(Comment)
+
+class CommentsDisplayGenericObjectAdmin(CommentsAdmin):
+    list_display = ('name', 'commented_object', 'ip_address',
+                    'submit_date', 'is_public', 'is_removed')
+
+    def commented_object(self, obj):
+        return '<a href="%s">%s</a>' % (self.commented_obj_url(obj.content_object), obj.content_object)
+    commented_object.allow_tags=True
+
+    def commented_obj_url(self, obj):
+        # hackish, needs to actually do a reverse lookup
+        return '/admin/%s/%s/%s/' % (obj._meta.app_label,
+                                     obj._meta.module_name,
+                                     obj.id)
+    
+admin.site.register(Comment, CommentsDisplayGenericObjectAdmin)
