ba51717297de0dc1d32bf0b1539b6a4676346b26
leahculver <leah.culver@gmail.com>
2009-03-11 14:35:27 -0700

    microformat date filter

--- apps/blog/templatetags/blog_utils.py
+++ apps/blog/templatetags/blog_utils.py
@@ -56,3 +56,12 @@ def show_post_brief(context, post):
         "can_edit": context["user"].is_staff,
     }
 register.inclusion_tag("blog/post_brief.html", takes_context=True)(show_post_brief)
+
+@register.filter
+def date_microformat(d):
+    '''
+        Microformat version of a date.
+        2009-02-10T02:58:00+00:00 (ideal)
+        2009-02-09T17:54:41.181868-08:00 (mine)
+    '''
+    return d.isoformat()
