77506dc893c8954ab15e8714b7b597ccdf02802f
Sean McQuillan <mcquillan.sean@gmail.com>
2009-03-30 03:28:13 +0800

    Refactored markdown code coloring to mdx_codehilit from the interwebs.
    mdx_codehilite taken from https://code.achinghead.com/browser/py-markdown-ext/codehilite/trunk/mdx_codehilite.py?rev=22
    
    This implementation overwrites the codeblock parsing function cleanly, allowing for detabbing and other markdown magic, while also ensuring <pre> tags end up placed correctly when at the end of a list or other enclosing tags.
    
    Signed-off-by: Leah Culver <leah.culver@gmail.com>

--- apps/blog/util/code_hilite.py
+++ apps/blog/util/code_hilite.py
@@ -8,6 +8,7 @@ from docutils.core import publish_parts
 from docutils.parsers.rst import directives
 
 import markdown
+from mdx_codehilite import makeExtension
 
 from django.utils.safestring import mark_safe
 
@@ -52,58 +53,16 @@ def rst_to_html(value):
 
 
 #markdown -> HTML with code hiliting
-# http://lethain.com/entry/2008/jan/09/cleanly-extending-python-markdown-syntax-highlight/
 
-class CodeBlockProcessor(markdown.Preprocessor):
-    """
-    Code from http://lethain.com/entry/2008/jan/09/cleanly-extending-python-markdown-syntax-highlight/
-    
-    This will cause a full scan of the input text for lines beginning with @@.  Blocks that fall between
-    @@ tags will be assumed to be code blocks and formatted with pygments, the language must be specified
-    after the tag.
-    """
-    def run(self, lines):
-        new_lines = []
-        seen_start = False
-        lang = None
-        block = []
-        for line in lines:
-            untouched = line
-            line = line.lstrip()
-            if line.startswith("@@") and not seen_start:
-                lang = line.strip("@@").strip()
-                seen_start = True
-            elif line.startswith("@@") and seen_start:
-                try:
-                    lexer = get_lexer_by_name(lang)
-                except (ValueError, IndexError):
-                        # no lexer found - use the text one instead of an exception
-                        lexer = TextLexer()
-                block[-1] = block[-1].rstrip()
-                content = "\n".join(block)
-                highlighted = highlight(content, lexer, HtmlFormatter())
-                new_lines.append("%s" % (highlighted))
-                lang = None
-                block = []
-                seen_start = False
-            elif seen_start is True:
-                block.append(line)
-            else:
-                new_lines.append(untouched)
-        return new_lines
-
-def extendMarkdown(md):
-    md.preprocessors.append(CodeBlockProcessor())
-    print md.preprocessors
-    
-    
+MD_HILITE_EXT = makeExtension()    
+MARKDOWN = markdown.Markdown()
+MD_HILITE_EXT.extendMarkdown(MARKDOWN)
 
 def markdown_to_html(text):
     """
     Convert markdown to HTML with code hiliting
     """
-    md = markdown.Markdown()
-    extendMarkdown(md)
+    md = MARKDOWN
     md.source = text
     return unicode(md)
 
