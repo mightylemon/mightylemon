d477865435abef3785a99f863633622a7799712d
Justin Lilly <justinlilly@gmail.com>
2009-03-29 17:01:29 -0400

    Adding in stats tracking code
    I think at first, this should be in the local_settings.py level, but
    with multiple blogs / authors, we'll likely move this to a per-blog
    level.

--- apps/blog/context_processors.py
+++ apps/blog/context_processors.py
@@ -1,5 +1,16 @@
+from django.conf import settings
 
 def blog(request):
     return {
         'blog': request.blog,
     }
+
+def stats(request):
+    if not settings.DEBUG:
+        return {
+            'STATS_CODE': settings.STATS_CODE,
+            }
+    return {
+        'STATS_CODE': "<!-- debug mode enabled / no stats tracking -->",
+        }
+
