4cc34ba9415a172ebf79399364023bcd9e4744a1
leahculver <leah.culver@gmail.com>
2009-03-31 15:46:02 -0700

    applying some of jlilly's older changes

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,6 +1,8 @@
 
 from django.contrib import admin
 from blog.models import Blog, BlogSettings, Post
+from django.contrib.comments.admin import CommentsAdmin
+from django.contrib.comments.models import Comment
 
 
 class BlogAdmin(admin.ModelAdmin):
@@ -17,3 +19,21 @@ class PostAdmin(admin.ModelAdmin):
 admin.site.register(Blog, BlogAdmin)
 admin.site.register(BlogSettings)
 admin.site.register(Post, PostAdmin)
+
+admin.site.unregister(Comment)
+
+class CommentsDisplayGenericObjectAdmin(CommentsAdmin):
+    list_display = ('name', 'commented_object', 'ip_address',
+                    'submit_date', 'is_public', 'is_removed')
+
+    def commented_object(self, obj):
+        return '<a href="%s">%s</a>' % (self.commented_obj_url(obj.content_object), obj.content_object)
+    commented_object.allow_tags=True
+
+    def commented_obj_url(self, obj):
+        # hackish, needs to actually do a reverse lookup
+        return '/admin/%s/%s/%s/' % (obj._meta.app_label,
+                                     obj._meta.module_name,
+                                     obj.id)
+  
+admin.site.register(Comment, CommentsDisplayGenericObjectAdmin)
